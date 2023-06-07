5afac35ca37e52d59403df81115e16de5925e866
Fornalczyk <fk46509@zut.edu.pl>
2023-06-07 19:47:40 +0200

    Add event_managment file

---
+++ code/event_management.py
@@ -0,0 +1,26 @@
+from objects.characters.Player import Player
+from objects.world_elements.Wall import Wall
+
+
+def collision_with_wall(player, wall):
+    if not wall.get_rotation(): # wall is vertical
+        player.set_act_speed(0)
+
+        # check on which side of the wall the player is
+        if player.get_positionx() > wall.get_positionx():
+            player.set_positionx(player.get_positionx() + 1)
+        else:
+            player.set_positionx(player.get_positionx() - 1)
+
+    else: # wall is horizontal
+        # check on which side of the wall the player is
+
+        if player.get_positiony() > wall.get_positiony():
+            pass
+            # Gracz stoi na obiekcie i powstrzymuje go to przed spadkiem swobodnym w dół
+            # należy rozważyć czy przy skoku kiedy gracz będzie lądował na powierzchni to czy nie powinien wytracać
+            # w penwym stopniu swojej prędkości
+        else:
+            pass
+            # Gracz odbija się "głową" od powierzchni sciany. Należy rozważyć czy nie zaimplementować mechaniki
+            # odbicia (np, dodanie predkosci do opadania)
